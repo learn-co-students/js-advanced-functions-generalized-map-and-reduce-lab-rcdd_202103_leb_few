080b018d93250c1372f51a293d39719d5c573ae6
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-28 20:40:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map (srcArray, fctn){
+  let arr =[];
+  for (let i=0; i<srcArray.length; i++){
+    let stg = srcArray[i]
+    arr.push(fctn(stg))
+  }
+  return arr;
+}
+
+function reduce(src, fnct, start){
+    let arr = (!!start) ? start : src[0];
+    let i = (!!start) ? 0 : 1
+
+    for (; i < src.length; i++){
+        arr = fnct(src[i], arr)
+    }
+    return arr;
+}
