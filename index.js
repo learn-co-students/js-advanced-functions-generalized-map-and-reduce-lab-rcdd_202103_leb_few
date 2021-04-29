3a44100db9bc30a0cd73f1f626c79a9e8c26a3f6
Salim Al Mersally <salim.almersally@lau.edu>
2021-04-29 23:56:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+function map(array, fun){
+  return array.map(fun);
+}
+
+function reduce(array, fun, startValue){
+  if(startValue){
+    return array.reduce(fun) + startValue;
+  }else {
+    return array.reduce(fun);
+  }
+}
