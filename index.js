15a1b5704011b537506c6c6cb26ada22f25612d0
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-03 10:25:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // Add your functions here
 
+const map = (arr, fx) => {
+    const newArray = [];
+    for(let x of arr){
+        newArray.push((fx)(x))
+    }
+    return newArray;
+}
 
-//const mapToNegativize = (sourceArray) => {}
-map([1,2,3], function(a){ return a * -1 });
+const reduce = (arr, fx, startingValue) => {
+    let value = (!!startingValue) ? startingValue : arr[0]
+    let i = (!!startingValue) ? 0 : 1
+
+    for (; i < arr.length; i++) {
+      value = fx(arr[i], value)
+    }
+
+    return value;
+  }
